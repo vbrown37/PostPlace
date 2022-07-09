7be0833710dfa57844e025470469311fd8e7a36e
Axzial <axzial@protonmail.com>
2022-07-09 03:48:01 +0200

    :sparkles: Refactor Routing Modules

--- src/main/webapp/src/app/shared/shared.module.ts
+++ src/main/webapp/src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
-
-
 @NgModule({
   declarations: [],
   imports: [
