a7f6de7033cc1be10938fd8cdbf3b25c2fa49751
Axzial <axzial@protonmail.com>
2022-07-09 05:43:45 +0200

    :construction: Add SecurityConfig

---
+++ src/main/java/io/stonks/postplace/config/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package io.stonks.postplace.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+}
